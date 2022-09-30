e84f86194020301178fd62bf8c988bb00f752c58
Sean Hackett <100976172+TropicalXU@users.noreply.github.com>
2022-09-30 09:28:15 -0400

    more bug fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,7 +36,7 @@ module.exports = function(grunt) {
                 options: {
                     watchTask: true,
                     server: {
-                        baseDir: './'
+                        baseDir: 'https://candylabs.layerlabs.io'
                     }
                 }
             }
