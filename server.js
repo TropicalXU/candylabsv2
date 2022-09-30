65c1c0063319d2e1460ef574ade3509c18203fd0
Sean Hackett <100976172+TropicalXU@users.noreply.github.com>
2022-09-30 10:52:40 -0400

    new updates

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.render('index.html')
+    res.render('/index.html')
 })
 
 const port = process.env.PORT || 3000
