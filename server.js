dfb66d69e72300d00562f47db2f61985f57716c5
Sean Hackett <100976172+TropicalXU@users.noreply.github.com>
2022-09-30 12:29:43 -0400

    new one

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
 const ejs = require('ejs');
 
 const app = express();
 
 app.engine('ejs', ejs);
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.json());
 
