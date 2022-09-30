21532c27e14ed92a3e7424d049c52cdf36706d8a
Sean Hackett <100976172+TropicalXU@users.noreply.github.com>
2022-09-30 12:25:31 -0400

    hello

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const ejs = require('ejs');
 
 const app = express();
 
-app.engine('html', ejs);
+app.engine('ejs', ejs);
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
@@ -17,7 +17,7 @@ app.use((err, req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('/index')
 });
 
 
