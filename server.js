d10ba00ab645d965033302a343a4ca478afac721
Sean Hackett <100976172+TropicalXU@users.noreply.github.com>
2022-09-30 13:15:22 -0400

    updates to new bug fixes

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
+const ejsMate = require('ejs-mate');
 
 const app = express();
 
-app.engine('ejs', ejs);
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')));
